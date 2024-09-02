01074db41549371c56eb69b23d118f2fc71f5013
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-09-02 17:30:23 +0200

    php-code #4
    id has been re-indexed for each room

---
+++ index4.php
@@ -0,0 +1,28 @@
+<?php 
+  $filePath = "roomList.json";
+  $roomListData = file_get_contents($filePath);
+  $roomList = json_decode($roomListData, true);
+
+  $roomId = isset($_GET['id']) ? (int)$_GET['id'] : null;
+  if ($roomId) {
+    $filteredRooms = array_filter($roomList, function($room) use ($roomId) {
+      return $room['id'] === $roomId;
+    });
+
+    $room = !empty($filteredRooms) ? reset($filteredRooms) : null;
+  } else {
+    $room = null;
+  }
+
+?>
+
+<?php if ($room): ?>
+    <h1>Room: <?= htmlspecialchars($room['name']) ?></h1>
+    <ul>
+      <li>Name: <?= $room['name'] ?></li>
+      <li>Number: <?= $room['number'] ?></li>
+      <li>Price: <?= $room['price_night'] ?></li>
+    </ul>
+<?php else: ?>
+    <h1>Room not found.</h1>
+<?php endif; ?>
